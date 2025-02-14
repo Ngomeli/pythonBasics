693d05e432b6cad7306648bfb392d30e55d2e53a
Ngomeli <joshuangomeli@gmail.com>
2025-02-14 13:42:25 +0300

    used add function to add two numbers

--- functions.py
+++ functions.py
@@ -1,11 +1,11 @@
 #Built in Library Functions
-y=max(56,78,90,23,12)
+#y=max(56,78,90,23,12)
 #print(y)
 
-x=min(56,78,90,23,12)
+#x=min(56,78,90,23,12)
 #print("The minimum value is", x)
 
-z=pow(10,30)
+#z=pow(10,30)
 #print("The result value is", z)
 
 #user-defined functions
@@ -23,3 +23,9 @@ z=pow(10,30)
 #def add(a,b):    
  #   print("The sum is", a+b)
 #add(3,4)
+def add(a,b):
+    return a+b
+a=int(input("Enter the first number: "))
+
+b=int(input("Enter the second number: "))
+print("The sum is", add(a,b))
