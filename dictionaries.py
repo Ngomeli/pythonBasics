bb43942ca1b914276d5e59090d46dcc81b67cf11
Ngomeli <joshuangomeli@gmail.com>
2025-02-19 10:23:02 +0300

    Working with Dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,8 @@
+#Definition of dictionary
+europe = {"Spain":"Madrid", "France":"Paris", "Germany":"Berlin"}
+#Additing Italy to europe dictionary
+europe["Italy"] = "Rome"
+
+#Printing the updated dictionary
+
+print(europe)
