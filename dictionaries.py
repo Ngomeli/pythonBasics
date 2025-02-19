69ddb9c1f8b89ca53537a6629c1e77e6a6779635
Ngomeli <joshuangomeli@gmail.com>
2025-02-19 12:27:56 +0300

    using for loop on dictionary

--- dictionaries.py
+++ dictionaries.py
@@ -1,8 +1,12 @@
 #Definition of dictionary
-europe = {"Spain":"Madrid", "France":"Paris", "Germany":"Berlin"}
+#europe = {"Spain":"Madrid", "France":"Paris", "Germany":"Berlin"}
 #Additing Italy to europe dictionary
-europe["Italy"] = "Rome"
+#europe["Italy"] = "Rome"
 
 #Printing the updated dictionary
 
-print(europe)
+#print(europe)
+
+wold = {"europe":30.55, "Africa":39.21, "Asia": 49.89}
+for key, value in wold.items():
+    print(f"{key}: {value} million people")
