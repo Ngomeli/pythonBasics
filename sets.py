1a482174fa3bca0aa480517059875e348a2bf784
Ngomeli <joshuangomeli@gmail.com>
2025-02-21 14:40:23 +0300

    working with sets

---
+++ sets.py
@@ -0,0 +1,13 @@
+x=[1,2,3,1,2,3,1,2,3]
+print(set(x))
+
+y=[1,1,6,6,6,6,6,8,8]
+
+print(set(y))
+
+z = [("Bird", "Cat", "Dog", "Dog", "Bird", "Bird")]
+print(set(z))
+
+animals = set(["Cow", "Fish", "Pig", "Horse"])
+animals.add ("Cat")
+print(animals)
